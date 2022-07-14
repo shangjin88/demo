798446bc9e1415876820248cc13f7ddee95bdbba
pengzz <pengzhaozun@bingosoft.net>
2022-07-14 23:03:15 +0800

    设计模式 option

---
+++ golang/design-mode/option/option.go
@@ -0,0 +1,63 @@
+package main
+
+//reference： https://coolshell.cn/articles/21146.html
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+)
+
+type Server struct {
+	Addr     string
+	Port     int
+	Protocol string
+	Timeout  time.Duration
+	MaxConns int
+	TLS      *tls.Config
+}
+
+type Option func(*Server)
+
+func Protocol(p string) Option {
+	return func(s *Server) {
+		s.Protocol = p
+	}
+}
+func Timeout(timeout time.Duration) Option {
+	return func(s *Server) {
+		s.Timeout = timeout
+	}
+}
+func MaxConns(maxconns int) Option {
+	return func(s *Server) {
+		s.MaxConns = maxconns
+	}
+}
+func TLS(tls *tls.Config) Option {
+	return func(s *Server) {
+		s.TLS = tls
+	}
+}
+
+func NewServer(addr string, port int, options ...func(server *Server)) (*Server, error) {
+	srv := Server{
+		Addr:     addr,
+		Port:     port,
+		Protocol: "tcp",
+		Timeout:  30 * time.Second,
+		MaxConns: 1000,
+		TLS:      nil,
+	}
+
+	for _, option := range options {
+		option(&srv)
+	}
+
+	return &srv, nil
+}
+
+func main() {
+	srv, _ := NewServer("127.0.0.1", 8080, Protocol("udp"), Timeout(5*time.Second))
+	fmt.Printf("srv: %v", srv)
+}
